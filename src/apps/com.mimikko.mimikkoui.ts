f3f6c5d6008459462c8ba99859c737aa66dd09ea
MengNianxiaoyao <2589141604@qq.com>
2025-04-12 10:51:34 +0800

    feat: 规则更新

--- src/apps/com.mimikko.mimikkoui.ts
+++ src/apps/com.mimikko.mimikkoui.ts
@@ -7,12 +7,14 @@ export default defineGkdApp({
     {
       key: 2,
       name: '更新提示',
+      desc: '点击[今日不再提醒]',
       matchTime: 10000,
       matchDelay: 3000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
+          activityIds: 'com.mimikko.app.feature.home.ui.HomeMainActivity',
           matches:
             '@TextView[text="今日不再提醒"] < View <3 View < View < ViewGroup < [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/19642049',
