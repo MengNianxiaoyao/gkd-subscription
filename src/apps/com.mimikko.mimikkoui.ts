40c5fa56d42e1fcc6452e59a5ab426d3aba7518b
MengNianxiaoyao <2589141604@qq.com>
2025-05-02 22:29:24 +0800

    fix(兽耳助手): 更新提示

--- src/apps/com.mimikko.mimikkoui.ts
+++ src/apps/com.mimikko.mimikkoui.ts
@@ -9,10 +9,11 @@ export default defineGkdApp({
       name: '更新提示',
       desc: '点击[今日不再提醒]',
       matchTime: 9000,
-      actionMaximum: 1,
+      actionMaximum: 2,
       resetMatch: 'app',
       rules: [
         {
+          action: 'clickNode',
           matches:
             '@TextView[text="今日不再提醒"] < View <3 View < View < ViewGroup < [id="android:id/content"]',
           snapshotUrls: 'https://i.gkd.li/i/19642049',
