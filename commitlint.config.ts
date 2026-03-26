c45b17c2a11ed4e514509864146a3f622f6063cd
MengNianxiaoyao <2589141604@qq.com>
2026-03-26 19:13:06 +0800

    依赖更新

--- commitlint.config.ts
+++
@@ -1,8 +0,0 @@
-import type { UserConfig } from '@commitlint/types';
-
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'subject-case': [0],
-  },
-} as UserConfig;
